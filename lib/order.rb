5312f2feb496bf22d95fb53ed7a1e0639e539159
Valeria Serci <balerty84@gmail.com>
2023-02-26 20:06:01 +0000

    implement receipt and total methods in order

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,22 @@
 class Order 
   def initialize
+    @cart = []
   end
 
   def add(dish)
     fail 'Add a dish' if dish == nil
-    fail 'Out of stock' 
+    fail 'Out of stock' if !dish.is_available?
+    @cart << dish
   end
 
   def receipt 
-    ['Total: 0£']
+     @cart + [total]
+  end
+
+private
+  def total
+    total = @cart.sum(&:price)
+    "Total: #{total}£"
   end
 
 end
